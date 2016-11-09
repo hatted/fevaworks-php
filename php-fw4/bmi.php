1c6dc1b328d3d19213b8b1af6fe500e7686a0f93
hatted <hatted@gmail.com>
2016-11-09 09:17:23 +0800

    Add files via upload

--- php-fw4/bmi.php
+++ php-fw4/bmi.php
@@ -1,10 +1,11 @@
 <?php
 //bmi.php
-$result=round($_POST['weight']/$_POST['height']/$_POST['height'], 2);
-echo "Your BMI is $result<BR>";
-if ($result>25){
+$bmi=round($_POST['weight']/$_POST['height']/$_POST['height'], 2);
+echo "Your BMI is $bmi<BR>";
+
+if ($bmi>25){
 	echo "You are too heavy";
-} else if ($result<18.5){
+} else if ($bmi<18.5){
 	echo "You are not heavy enough";
 } else {
 	echo "You are fit";
