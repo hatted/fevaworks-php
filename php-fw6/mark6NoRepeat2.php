aa03795a293c8612ff7965b79ab2a268ca235ef8
hatted <hatted@gmail.com>
2016-11-09 09:16:44 +0800

    Add files via upload

--- php-fw6/mark6NoRepeat2.php
+++ php-fw6/mark6NoRepeat2.php
@@ -1,10 +1,17 @@
 <?php
 //mark6NoRepeat2.php
 $mark6=range(1,49);
+/*
+for ($i=0; $i<=49; $i++){
+	$mark6[$i]=$i;
+}
+*/
+
 shuffle($mark6);
-for ($j=1; $j<7; $j++){
+for ($j=0; $j<=5; $j++){
 	$result[$j]=$mark6[$j];
 }
+
 sort($result);
 print_r($result);
 ?>
