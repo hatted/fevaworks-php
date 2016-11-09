f75cf742d9dd82ddd79b0099041ca317401e7171
hatted <hatted@gmail.com>
2016-11-09 17:40:47 +0800

    Add files via upload

--- php-fw2/constant.php
+++ php-fw2/constant.php
@@ -1,11 +1,12 @@
 <?php
 //constant.php
 define("PI", 3.1416);
-
 $r=10;  	//radius
-$p=$r*2*PI;	//no $ for constant
-echo "p = ".$p;
+
+$c=$r*2*PI;	//no $ for constant
+echo "c = ".$c;
 echo "<p>";
+
 $a=$r*$r*PI;
 echo "Area = ".$a;
 ?>
