f4965572a0eb144eb3919c75c4b6d62dc7d2d1dc
hatted <hatted@gmail.com>
2016-11-09 17:39:30 +0800

    Add files via upload

--- php-fw1/birthday.php
+++ php-fw1/birthday.php
@@ -1,8 +1,10 @@
 <?php
 //birthday.php
-$bday=19900517;
+date_default_timezone_set('Asia/Hong_Kong');
+$name="Raymond";
+$birthday=19900517;
 $today=date("Ymd");
-$age=floor(($today-$bday)/(10000));
+$age=floor(($today-$birthday)/(10000));
 
 echo "You are $age years old";
 ?>
