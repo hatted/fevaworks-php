27cc4e914bd0b16934ae601a14d45c20997e2dc6
hatted <hatted@gmail.com>
2016-11-29 22:18:09 +0800

    products

--- php-fw9/products.php
+++ php-fw9/products.php
@@ -3,40 +3,40 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>products list</title>
-<link href="style.css" rel="stylesheet" type="text/css">
+<title>Untitled Document</title>
 <link href="products.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-
 <?php
-//products.php
+//products_readall.php
 $query="SELECT * FROM `products` ORDER BY `products`.`id` DESC";
 $recordset=$mysqli->query($query);
 mysqli_fetch_all($recordset, MYSQLI_ASSOC);
 ?>
 
-<div class="products_list">
+
+
 <?php
 foreach($recordset as $record){
 ?>
+<a href="product_item.php?id=<?php echo $record['id']; ?>">
 <div class="product">
-    <a href="product.php?id=<?php echo $record['id']; ?>">
-        <div class="product_image">
-        	<img src="<?php echo $record['image']; ?>">
-        </div>
-        <div class="product_name">
-	        <?php echo $record['name']; ?>
-        </div>
-    </a>
+	<div class="product_image">
+		<img src="<?php echo $record['image']; ?>" width="100" height="100">
+    </div>
+	<div class="product_name">
+		<?php echo $record['name']; ?>
+    </div>
+	<div class="product_price">
+    	<span class="price_o">$<?php echo $record['price']; ?></span>
+		<span>$<?php echo $record['price']; ?></span>
+    </div>
 </div>
+</a>
 <?php } ?>
-</div>
 
-
-
-
+<p>&nbsp;</p>
 </body>
 </html>
 <?php
