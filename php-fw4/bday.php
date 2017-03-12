e149d90e48ae6dd8f738b41b0e1dc53fdb1dfaed
hatted <hatted@gmail.com>
2017-03-12 17:29:20 +0800

    date default timezone

--- php-fw4/bday.php
+++ php-fw4/bday.php
@@ -1,5 +1,6 @@
 ﻿<?php
 //bday.php
+date_default_timezone_set('Asia/Hong_Kong');
 $bday=$_POST['year'].$_POST['month'].$_POST['day'];
 $today=date("Ymd");
 $age=floor(($today-$bday)/(10000));
--- php-fw4/birthday.php
+++ php-fw4/birthday.php
@@ -1,5 +1,6 @@
 <?php
 //birthday.php
+date_default_timezone_set('Asia/Hong_Kong');
 $bday=19900517;
 $today=date("Ymd");
 $age=floor(($today-$bday)/(10000));
